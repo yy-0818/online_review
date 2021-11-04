20058f84bf857d19af83a08d7c836d4043487932
YOU <8127881+torUntitled@user.noreply.gitee.com>
2021-11-04 18:34:25 +0800

    ui

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,67 +1,78 @@
 <template>
-  <div style="width: 100%; height: 100vh; overflow: hidden">
+  <div
+    style="width: 100%; height: 100vh; overflow: hidden;background: linear-gradient(90deg,#be8dd8,#763ab6);"
+  >
     <!-- <img src="../assets/img/fc.png" alt="fj" /> -->
     <div style="width: 400px; margin: 100px auto">
-      <div style="font-size: 30px; text-align: center; padding: 30px 0">
-        欢迎登录
-      </div>
-      <el-form ref="form" :model="form" size="normal" :rules="rules">
-        <el-form-item prop="username">
-          <el-input
-            prefix-icon="el-icon-user-solid"
-            v-model="form.username"
-            placeholder="请输入账号(邮箱)"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            prefix-icon="el-icon-lock"
-            v-model="form.password"
-            show-password
-            placeholder="请输入密码"
-          ></el-input>
-        </el-form-item>
-
-        <el-form-item>
-          <div style="display: flex">
+      <el-card>
+        <div style="font-size: 30px; text-align: center; padding: 30px 0">
+          欢迎登录
+        </div>
+        <el-form ref="form" :model="form" size="normal" :rules="rules">
+          <el-form-item prop="username">
             <el-input
-              prefix-icon="el-icon-key"
-              v-model="form.validCode"
-              style="width: 65%"
-              placeholder="请输入验证码"
+              prefix-icon="el-icon-user-solid"
+              v-model="form.username"
+              placeholder="请输入用户名(邮箱)"
             ></el-input>
-            <ValidCode style="margin-left: 8px" @input="createValidCode" />
-          </div>
-        </el-form-item>
-        <el-form-item>
-          <el-radio v-model="form.role" :label="3">管理员</el-radio>
-          <el-radio v-model="form.role" :label="2">审核员</el-radio>
-          <el-radio v-model="form.role" :label="1">普通用户</el-radio>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input
+              prefix-icon="el-icon-lock"
+              v-model="form.password"
+              show-password
+              placeholder="请输入密码"
+            ></el-input>
+          </el-form-item>
 
-          <!--          <el-button @click="resetForm('ruleForm')" >重置</el-button>-->
-        </el-form-item>
-        <el-form-item>
-          <el-button style="width: 100%" type="primary" @click="login"
-            >登 录</el-button
+          <el-form-item>
+            <div style="display: flex">
+              <el-input
+                prefix-icon="el-icon-key"
+                v-model="form.validCode"
+                style="width: 65%"
+                placeholder="请输入验证码"
+              ></el-input>
+              <ValidCode style="margin-left: 8px" @input="createValidCode" />
+            </div>
+          </el-form-item>
+          <el-form-item>
+            <el-radio v-model="form.role" :label="3">管理员</el-radio>
+            <el-radio v-model="form.role" :label="2">审核员</el-radio>
+            <el-radio v-model="form.role" :label="1">普通用户</el-radio>
+
+            <!--          <el-button @click="resetForm('ruleForm')" >重置</el-button>-->
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              style="width: 100%"
+              type="primary"
+              :loading="true"
+              @click="login"
+              >登 录</el-button
+            >
+          </el-form-item>
+          <el-form-item
+            ><el-button type="text" @click="$router.push('/register')"
+              >前往注册 >>
+            </el-button></el-form-item
           >
-        </el-form-item>
-        <el-form-item
-          ><el-button type="text" @click="$router.push('/register')"
-            >前往注册 >>
-          </el-button></el-form-item
-        >
-      </el-form>
+        </el-form>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
 import request from "@/utils/request";
+
+// import Loading from "@/components/loading";
 import ValidCode from "@/components/ValidCode";
 
 export default {
   name: "Login",
   components: {
+    Loading,
     ValidCode,
   },
   data() {
@@ -132,4 +143,13 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.el-card {
+  min-width: 380px;
+  margin-right: 20px;
+  transition: all 0.5s;
+}
+.el-card:hover {
+  margin-top: -5px;
+}
+</style>
