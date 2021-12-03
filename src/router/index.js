5d81779bdd46e691bef96f4243fa85258c2558ba
YOU <8127881+torUntitled@user.noreply.gitee.com>
2021-12-03 14:43:32 +0800

    style:增加动态title

--- src/router/index.js
+++ src/router/index.js
@@ -13,27 +13,47 @@ const routes = [
         path: "home",
         name: "Home",
         component: () => import("@/views/Home"),
+        meta: {
+          title: "首页",
+          keepAlive: true,
+        },
       },
       {
         path: "user",
         name: "User",
         component: () => import("@/views/User"),
+        meta: {
+          title: "用户管理",
+          keepAlive: true,
+        },
       },
       {
         path: "paper",
         name: "Paper",
         component: () => import("@/views/Paper"),
+        meta: {
+          title: "论文上传",
+          keepAlive: true,
+        },
       },
 
       {
         path: "news",
         name: "News",
         component: () => import("@/views/News"),
+        meta: {
+          title: "资源分享",
+          keepAlive: true,
+        },
       },
       {
         path: "person",
         name: "Person",
-        component: () => import("@/views/Person"), //个人中心
+        component: () => import("@/views/Person"),
+        meta: {
+          title: "个人中心",
+          keepAlive: true,
+        },
       },
 
       {
@@ -50,16 +70,28 @@ const routes = [
         path: "Reviewer",
         name: "Reviewer",
         component: () => import("@/views/Reviewer"),
+        meta: {
+          title: "初审",
+          keepAlive: true,
+        },
       },
       {
         path: "Reviewer2",
         name: "Reviewer2",
         component: () => import("@/views/Reviewer2"),
+        meta: {
+          title: "终审",
+          keepAlive: true,
+        },
       },
       {
         path: "MyPaper",
         name: "MyPaper",
         component: () => import("@/views/MyPaper"),
+        meta: {
+          title: "我的论文",
+          keepAlive: true,
+        },
       },
     ],
   },
@@ -72,17 +104,29 @@ const routes = [
     path: "/login",
     name: "Login",
     component: () => import("@/views/Login"),
+    meta: {
+      title: "登录_科研成果内部审核系统",
+      keepAlive: true,
+    },
   },
   {
     path: "/register",
     name: "Register",
     component: () => import("@/views/Register"),
+    meta: {
+      title: "注册_科研成果内部审核系统",
+      keepAlive: true,
+    },
   },
 
   {
     path: "/:catchAll(.*)",
     name: "/404",
     component: () => import("../views/404.vue"),
+    meta: {
+      title: "404",
+      keepAlive: true,
+    },
   },
 ];
 
@@ -95,6 +139,11 @@ const router = createRouter({
 let limitPagePath = ["/about"];
 
 router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
   if (limitPagePath.includes(to.path)) {
     // 判断sessionStorage是否保存了用户信息
     let userStr = sessionStorage.getItem("user") || "{}";
