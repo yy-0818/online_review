3bd95c81aa32b8b1b0e8d27189efcbce16877e65
YOU <8127881+torUntitled@user.noreply.gitee.com>
2022-02-28 10:17:36 +0800

    feat: 新增归档页面(构建中)

---
+++ src/views/Archive/ArchiveArticle.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-skeleton :rows="5" animated />
+  <el-empty description="description"></el-empty>
+  <el-skeleton :rows="5" animated />
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+<style></style>
