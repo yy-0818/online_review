2ff23b6d981cafc11612689dc57f07358b415e7f
YOU <534764132@qq.com>
2022-11-18 21:19:54 +0800

    feat:change logo

--- src/layout/components/Logo.vue
+++ src/layout/components/Logo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="el-logo">
     <!-- <img src="@/assets/logo.png" alt="" /> -->
-    <img src="http://static.ivanlife.cn//imges/lo.png" alt="" />
+    <img src="http://static.ivanlife.cn/imges/title.png" alt="logo" />
     <span class="title" :class="{ 'is-hide': isHideTitle }">在线审核系统</span>
   </div>
 </template>
